47a013773bb89a1e873b73b835181912b5aba096
cjlee38 <cjlee38@github.com>
2022-06-11 18:46:26 +0900

    chore: frontend 코드베이스 변경

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 const axios = Axios.create({
   baseURL:
-    'http://ec2-13-125-246-80.ap-northeast-2.compute.amazonaws.com:8080/api',
+    'http://localhost:8080/api',
 });
 
 export default axios;
